08edf1cc437056d29e4903a5b80e469a1f576022
Akashi <akashi_sai@163.com>
2023-06-09 08:02:09 +0800

    :sparkles: feat: RSS-Hub

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-export const one = 1
-export const two = 2
+import rssHub from 'rsshub'
+
+rssHub.init()
+
+rssHub.request('/bilibili/bangumi/media/9192')
+  .then((data: any) => {
+    console.log(data)
+  })
+  .catch((e: any) => {
+    console.log(e)
+  })
